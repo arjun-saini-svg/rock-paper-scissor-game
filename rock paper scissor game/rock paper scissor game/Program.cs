bac925feaae23adf538e2379ac3e2c7a97e764bd
arjun-saini-svg <asphcasee@gmail.com>
2025-09-20 23:08:42 +0530

    logic for programs choice

--- rock paper scissor game/rock paper scissor game/Program.cs
+++ rock paper scissor game/rock paper scissor game/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        bool playagain = true;
+        string userChoice;
+        string programChoice;
+        int programInput;
+
+        while (playagain)
+        {
+            programInput = random.Next(1,4);
+
+            programChoice = programInput switch
+            {
+                1 => "rock",
+                2 => "paper",
+                3 => "scissor",
+            };
+
+            Console.WriteLine("Enter rock, paper or scissor:");
+            userChoice = Console.ReadLine();
+
+            
+        }
+
+        Console.ReadKey();
+    }
+}
