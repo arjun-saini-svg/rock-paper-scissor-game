ddd31e5210dd07c468edb7fe4d916e1f410e36f4
arjun-saini-svg <asphcasee@gmail.com>
2025-09-20 23:25:46 +0530

    main logic

--- rock paper scissor game/rock paper scissor game/Program.cs
+++ rock paper scissor game/rock paper scissor game/Program.cs
@@ -9,6 +9,7 @@ class Program
         string userChoice;
         string programChoice;
         int programInput;
+        string result;
 
         while (playagain)
         {
@@ -19,12 +20,29 @@ class Program
                 1 => "rock",
                 2 => "paper",
                 3 => "scissor",
+                _ => "idk"
+            };
+
+            switch (programChoice)
+            {
+                case "idk":
+                    Console.WriteLine("program didn't choose");
+                        break;
             };
 
             Console.WriteLine("Enter rock, paper or scissor:");
             userChoice = Console.ReadLine();
 
-            
+            result = (userChoice, programChoice) switch
+            {
+                ("rock", "paper") => "You lose",
+                ("rock", "scissor") => "You win",
+                ("paper", "scissor") => "You lose",
+                ("paper", "rock") => "You win",
+                ("scissor", "paper") => "You win",
+                ("scissor", "rock") => "You lose",
+                var (u, p) when u == p => "Its a TIE",
+            };
         }
 
         Console.ReadKey();
